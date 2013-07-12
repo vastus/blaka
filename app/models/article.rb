b51a8b7b826a6a840310d143e09d139ff0dc635f
mnkd <monkd@multi.fi>
2013-07-13 02:02:27 +0300

    article belongs to category

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,9 @@ class Article < ActiveRecord::Base
   # attrs
   attr_accessible :extended, :intro, :title
 
+  # relationships
+  belongs_to :category
+
   # validations
   validates_presence_of :title, :intro, :extended
   validates_uniqueness_of :title
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130712175019) do
+ActiveRecord::Schema.define(:version => 20130712225933) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
     t.text     "intro"
     t.text     "extended"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.integer  "category_id"
   end
+
+  add_index "articles", ["category_id"], :name => "index_articles_on_category_id"
 
 end
