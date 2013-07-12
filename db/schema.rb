42b6b473fd34eb8cc930df4b85465fbe86a2457c
mnkd <monkd@multi.fi>
2013-07-13 02:03:23 +0300

    created category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130712225933) do
+ActiveRecord::Schema.define(:version => 20130712230245) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -23,5 +23,11 @@ ActiveRecord::Schema.define(:version => 20130712225933) do
   end
 
   add_index "articles", ["category_id"], :name => "index_articles_on_category_id"
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
