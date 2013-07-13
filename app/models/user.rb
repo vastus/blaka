24a99f27d69ec713563c3d6deac1a607c90e7046
mnkd <monkd@multi.fi>
2013-07-13 03:26:07 +0300

    added before save callback to hash pass on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
   # attrs
   attr_accessible :name, :email, :password_hash, :password_salt
@@ -5,9 +7,21 @@ class User < ActiveRecord::Base
   # virtual attrs
   attr_accessor :password
 
+  # callbacks
+  before_save :generate_password
+
   # validations
   validates_presence_of :name, :email
   validates_presence_of :password, on: :create
   validates_confirmation_of :password
+  # validates_length_of :password
+
+  private
+  def generate_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
 end
 
