fb467d948d24e39582e40f1c941abb1fe036ac7f
mnkd <monkd@multi.fi>
2013-07-13 20:45:34 +0300

    sessions#create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url
+    else
+      flash.now.alert = 
+        I18n.t('login.incorrect_login.flash', default: 'Wrong email or password.')
+      render :new
+    end
+  end
 end
