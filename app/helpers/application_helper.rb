373f80b828c75f76dcc1d71ba4fac6641d979d23
mnkd <monkd@multi.fi>
2013-07-13 13:54:42 +0300

    link to category helper + styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def link_to_category(cat)
+    html_options = {}
+    if current_page? cat
+      html_options[:class] = 'active'
+    end
+    html_options[:title] = t('categories.link.title', default: "View all articles under #{cat.name.capitalize}")
+    link_to cat.name.capitalize, cat, html_options
+  end
 end
