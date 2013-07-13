74ceda81e03ce8d27e6472624ac5357647cd28fe
mnkd <monkd@multi.fi>
2013-07-13 03:16:09 +0300

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password_hash, :password_salt
+  # attrs
+  attr_accessible :name, :email, :password_hash, :password_salt
+
+  # virtual attrs
+  attr_accessor :password
+
+  # validations
+  validates_presence_of :name, :email
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password
 end
+
