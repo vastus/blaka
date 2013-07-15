e5555c142d1062afd59d310328ef57d1b3687b51
juho H <juhoth@ovi.com>
2013-07-15 03:48:12 +0300

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+code_cat = Category.create!(
+  name: 'code'
+)
+
+work_cat = Category.create!(
+  name: 'work'
+)
+
+testos = User.create!(
+  name: 'Testos Teroni',
+  email: 'testos@teroni.fi',
+  password: 'secretos',
+  role: 'admin'
+)
+
+art1 = code_cat.articles.build(
+  title: "Handling Signals",
+  intro: "The way that you define signal-specific behaviour is to ‘trap’ the signal, specifying some behaviour that should be executed when the signal is received.",
+  extended: "Try running this program. On the second last line, the current process id (pid) is printed. You can plug this in to the kill(1) command to see what happens when you send signals to your process.",
+  user_id: testos.id,
+  category_id: code_cat.id
+)
+
+art2 = work_cat.articles.build(
+  title: "Doing a Paf gig on XPRS",
+  intro: "First time on Viking Line doing a Paf gig. XPRS goes between HEL&mdash;TAL.",
+  extended: "This gig is pretty awesome. You just have to adapt to different situations and control the moment.",
+  user_id: testos.id,
+  category_id: work_cat.id
+)
+
+art1.save!
+art2.save!
+
+# comment1 = art1.comments.build(
+# )
+# 
+# comment2 = art1.comments.build(
+# )
+# 
+# comment1.save!
+# comment2.save!
+
