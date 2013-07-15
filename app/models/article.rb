a6ef354f95c3f7274a64fa6b6844bb527063bc35
juho H <juhoth@ovi.com>
2013-07-15 03:49:30 +0300

    add user_id to attrs and validate its presence

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,13 @@
 class Article < ActiveRecord::Base
   # attrs
-  attr_accessible :extended, :intro, :title, :category_id
+  attr_accessible :extended, :intro, :title, :category_id, :user_id
 
   # relationships
   belongs_to :category
   belongs_to :author, foreign_key: :user_id, class_name: 'User'
 
   # validations
-  validates_presence_of :title, :intro, :extended, :category_id
+  validates_presence_of :title, :intro, :extended, :category_id, :user_id
   validates_uniqueness_of :title
 end
 
