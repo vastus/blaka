75c820e24d0ad1c7c927e3fef20c85a7625a16e4
juho H <juhoth@ovi.com>
2013-07-19 00:00:32 +0300

    authorize actions

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,10 +1,12 @@
 class Admin::ArticlesController < ApplicationController
   def new
     @article = Article.new
+    authorize! :create, @article
   end
 
   def create
     @article = Article.new(params[:article])
+    authorize! :create, @article
     if @article.save
       # flash it. necessary?
       redirect_to article_url(@article)
@@ -16,10 +18,12 @@ class Admin::ArticlesController < ApplicationController
 
   def edit
     @article = Article.find(params[:id])
+    authorize! :update, @article
   end
 
   def update
     @article = Article.find(params[:id])
+    authorize! :update, @article
     if @article.update_attributes(params[:article])
       redirect_to article_url(@article)
     else
---
+++ app/models/ability.rb
@@ -0,0 +1,39 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+
+    if user.role? :admin
+      can :manage, :all
+    else
+      can :read, :all
+      can :create, Comment
+      can :edit, Comment do |comment|
+        comment.try(:user) == user
+      end
+      if user.role? :author
+        can :create, Article
+        can :update, Article do |article|
+          article.try(:user) == user
+        end
+      end
+    end
+    
+    # If you pass :manage it will apply to every action. Other common actions
+    # here are :read, :create, :update and :destroy.
+    #
+    # The second argument is the resource the user can perform the action on. 
+    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
+    # class of the resource.
+    #
+    # The third argument is an optional hash of conditions to further filter the
+    # objects.
+    # For example, here the user can only update published articles.
+    #
+    #   can :update, Article, :published => true
+    #
+    # See the wiki for details:
+    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
+  end
+end
